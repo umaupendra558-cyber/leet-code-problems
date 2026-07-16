d0fa4fc134f3dd4d9984e73507142ddb6cab0a0c
umaupendra558-cyber <umaupendra558@gmail.com>
2026-07-16 11:40:41 +0530

    Create Day-1-7,Reverse Integer.java

---
+++ Day-1-7,Reverse Integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            
+            // Check for overflow before multiplying by 10
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            // Check for underflow before multiplying by 10
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            
+            rev = rev * 10 + pop;
+        }
+        
+        return rev;
+    }
+}
